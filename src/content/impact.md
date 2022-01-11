1270cd61ca47fb864ef745826de028b6f2acdb64
Seth A. Johnson <sethajoh@umich.edu>
2022-01-11 13:48:53 -0500

    FULCRUMOPS-106 Embed public Kibana board to EBC Aboutware

--- src/content/impact.md
+++ src/content/impact.md
@@ -8,9 +8,8 @@ readershipMapDescription: >-
   The Readership Map replays every view or download of UMP EBC books or media
   components that occurred the previous four weeks.
 googleDataStudioDescription: >-
-  ## Book Usage
+  ## All Book Usage
 
 
-  The Google Analytics report shows basic usage data including users, sessions,
-  pageviews, popular titles, and user location.
+  The Google Analytics report shows basic usage data from the Fulcrum platform. This includes users, sessions, pageviews, popular titles, and user location for titles in EBC. For more information, see [our FAQ](https://mpub.atlassian.net/wiki/spaces/FPS/pages/66453810/UMP+EBC).
 ---
--- src/templates/impact-page.js
+++ src/templates/impact-page.js
@@ -36,20 +36,20 @@ const Impact = ({data}) => {
           <div className="readership-map">
             <ReactMarkdown source={readershipMapDescription} />
             <div className="readership-map-embed">
-              <iframe title="UMP EBC Readership Map" frameborder="0" height="650" width="100%" src="https://maps.publishing.umich.edu/readership-map/?filter.view=132442623"></iframe>
+              <iframe title="Readership Map" frameborder="0" height="650" width="100%" src="https://maps.publishing.umich.edu/readership-map/?filter.view=132442623"></iframe>
+            </div>
+          </div>
+          <div className="google-data-studio-oa">
+            <h2>Open Access Book Usage</h2>
+            <p>This report is created by the <a href="https://educopia.org/data_trust/" target="_blank" rel="noreferrer">Open Access Ebook Usage Trust</a> (OAeBU) and represents usage across a number of different sources platforms. For more information, see <a href="https://mpub.atlassian.net/wiki/spaces/FPS/pages/66453810/UMP+EBC" target="_blank" rel="noreferrer">our FAQ</a></p>
+            <div className="embed-responsive embed-responsive-1by1">
+              <iframe title="Open Access Book Usage" className="embed-responsive-item" src="https://oaebu-u-m-press.observatory.academy/s/u-m-press/app/dashboards#/view/f6de4970-0686-11ec-a271-a937a93ef099?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:'2006-01-01',to:now%2B2y))&embed=true"></iframe>
             </div>
           </div>
           <div className="google-data-studio">
             <ReactMarkdown source={googleDataStudioDescription} />
             <div className="embed-responsive embed-responsive-1by1">
-              <iframe title="UMP EBC Usage Report" className="embed-responsive-item" src="https://datastudio.google.com/embed/reporting/1fFXJLWMpoGSz_RNTDCKDroEAxjk-wRHw/page/9LCE"></iframe>
-            </div>
-          </div>
-          <div className="google-data-studio-oa">
-            <h2>Open Access Ebooks</h2>
-            <p>This report is created by the <a href="https://educopia.org/data_trust/" target="_blank">Open Access Ebook Usage Trust</a> (OAeBU) and represents usage across a number of different sources platforms. For more information, see <a href="https://tools.lib.umich.edu/confluence/display/FPS/UMP+EBC+Usage+Statistics+FAQ" target="_blank">our FAQ</a></p>
-            <div className="embed-responsive embed-responsive-1by1">
-              <iframe title="UMP EBC Open Access Usage" className="embed-responsive-item" src="https://datastudio.google.com/embed/u/0/reporting/9b42ed06-ab37-4bb5-9215-4ea8c3ee70ec/page/9i1ZC"></iframe>
+              <iframe title="All Book Usage Report" className="embed-responsive-item" src="https://datastudio.google.com/embed/reporting/1fFXJLWMpoGSz_RNTDCKDroEAxjk-wRHw/page/9LCE"></iframe>
             </div>
           </div>
         </section>
