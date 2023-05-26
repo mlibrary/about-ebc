22c5c5274898323ef5df5bc5cf87465487ba0f8d
jmcglone <jmcglone@umich.edu>
2023-05-26 10:32:40 -0400

    update to use github for cms auth

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -14,14 +14,9 @@ if (process.env.BRANCH === "preview") {
   CMS.init({
     config: {
       backend: {
-        name: 'gitlab',
-        repo: 'michigan-publishing/about-ebc',
-        branch: 'preview',
-        auth_type: 'implicit',
-        app_id: '',
-        api_root: 'https://gitlab.umich.edu/api/v4',
-        base_url: 'https://gitlab.umich.edu',
-        auth_endpoint: 'oauth/authorize'
+        name: 'github',
+        repo: 'mlibrary/about-ebc',
+        branch: 'preview'
       }
     }
   });
@@ -29,14 +24,9 @@ if (process.env.BRANCH === "preview") {
   CMS.init({
     config: {
       backend: {
-        name: 'gitlab',
-        repo: 'michigan-publishing/about-ebc',
-        branch: 'master',
-        auth_type: 'pkce',
-        app_id: 'e4b04eff05cb0b43b098d621936c4a168b0159980b5b1a5196f54eaf23bb254e',
-        api_root: 'https://gitlab.umich.edu/api/v4',
-        base_url: 'https://gitlab.umich.edu',
-        auth_endpoint: 'oauth/authorize'
+        name: 'github',
+        repo: 'mlibrary/about-ebc',
+        branch: 'master'
       }
     }
   });
