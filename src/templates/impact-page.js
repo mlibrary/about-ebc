013db6f43c5dccf96bf8f785b9fe539322f10fb4
jmcglone <jmcglone@umich.edu>
2024-08-30 16:25:32 -0400

    add readership map back; update embed url for ga4 data

--- src/templates/impact-page.js
+++ src/templates/impact-page.js
@@ -33,6 +33,12 @@ const Impact = ({data}) => {
           </Link>
         </section>
         <section>
+        <div id="readership-map" className="readership-map">
+            <ReactMarkdown source={readershipMapDescription} />
+            <div className="readership-map-embed">
+              <iframe title="Readership Map" frameborder="0" height="650" width="100%" src="https://maps.publishing.umich.edu/readership-map/?filter.stream=4561158127"></iframe>
+            </div>
+          </div>
           <div className="google-data-studio-oa">
             <h2 id="oa-book-usage">Open Access Book Usage</h2>
             <p>This report is created by the <a href="https://openknowledge.community/projects/bad-project/" target="_blank" rel="noreferrer">Book Analytics Dashboard project</a> and represents usage across a number of different platforms. For more information or if you are encountering display issues, see <a href="https://mpub.atlassian.net/wiki/spaces/FPS/pages/66453810/UMP+EBC" target="_blank" rel="noreferrer">our FAQ</a></p>
